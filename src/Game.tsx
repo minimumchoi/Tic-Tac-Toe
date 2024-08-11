6cc9843d001eafa23b95cd5c9c29d098d03b162c
채소 <chlthgus0901@naver.com>
2024-08-11 16:09:13 +0900

    game으로 상태 끌어올리기

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
 import Board from "./components/Board/Board";
 import History from "./components/History/History";
-import "./styles/main.css";
+import {
+  checkWinner,
+  PLAYER_COUNT,
+  PLAYERS,
+  INITIAL_SQUARES,
+} from "./constants";
 import S from "./Game.module.css";
+import "./styles/main.css";
 
 export function Game() {
+  const [gameHistory, setHisory] = useState(INITIAL_SQUARES);
+
+  const gameIndex = gameHistory.filter(Boolean).length;
+  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0;
+  const nextPlayer = isPlayerOneTurn ? PLAYERS.ONE : PLAYERS.TWO;
+
+  const handleClick = (index: number) => () => {
+    const winnerInfo = checkWinner(gameHistory);
+    if (winnerInfo) {
+      alert("게임끝 그만 누르세요~~");
+      return;
+    }
+
+    setHisory(
+      gameHistory.map((square, squaresIndex) => {
+        if (squaresIndex === index) {
+          return nextPlayer;
+        }
+        return square;
+      })
+    );
+  };
+
+  const winnerInfo = checkWinner(gameHistory);
+  const isDraw = !winnerInfo && gameHistory.every(Boolean);
   return (
     <div className={S.component}>
-      <Board />
+      <Board
+        nextPlayer={nextPlayer}
+        winnerInfo={winnerInfo}
+        isDraw={isDraw}
+        squares={gameHistory}
+        onClick={handleClick}
+      />
       <History />
     </div>
   );
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,45 +1,27 @@
-import {
-  checkWinner,
-  INITIAL_SQUARES,
-  PLAYER_COUNT,
-  PLAYERS,
-} from "../../constants";
+import { checkWinner, PLAYER_COUNT, PLAYERS } from "../../constants";
 import Squares from "../Squares/Squares";
 import Status from "../Status/Status";
 import S from "./Board.module.css";
-import { useState } from "react";
 
-function Board() {
-  const [squares, setSquares] = useState(INITIAL_SQUARES);
+interface BoardProps {
+  nextPlayer: string;
+  winnerInfo: object | null;
+  isDraw: boolean;
+  squares: Array<string>;
+  onClick: (index: number) => void;
+}
 
-  const gameIndex = squares.filter(Boolean).length;
-  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0;
-  const nextPlayer = isPlayerOneTurn ? PLAYERS.ONE : PLAYERS.TWO;
-
-  const handleClick = (index: number) => () => {
-    const winnerInfo = checkWinner(squares);
-    if (winnerInfo) {
-      alert("게임끝 그만 누르세요~~");
-      return;
-    }
-
-    setSquares(
-      squares.map((square, squaresIndex) => {
-        if (squaresIndex === index) {
-          return nextPlayer;
-        }
-        return square;
-      })
-    );
-  };
-
-  const winnerInfo = checkWinner(squares);
-  const isDraw = !winnerInfo && squares.every(Boolean);
-
+function Board({
+  nextPlayer,
+  winnerInfo,
+  isDraw,
+  squares,
+  onClick,
+}: BoardProps) {
   return (
     <div className={S.component}>
       <Status nextPlayer={nextPlayer} winner={winnerInfo} isDraw={isDraw} />
-      <Squares squares={squares} onClick={handleClick} />
+      <Squares squares={squares} onClick={onClick} />
     </div>
   );
 }
